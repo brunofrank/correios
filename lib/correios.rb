f490f5060290b925971a5c36d53a2e3992091332
Alexandre de Oliveira <chavedomundo@gmail.com>
2012-08-11 01:39:32 -0300

    Adds tests for shipping tracking
    Adds fixture of the shipping status page and loads it in the test,
    stubbing Nokogiri.

--- lib/correios.rb
+++ lib/correios.rb
@@ -42,7 +42,7 @@ class Correios
     WEBSRO = "http://websro.correios.com.br/sro_bin/txect01$.QueryList?P_LINGUA=001&P_TIPO=001&P_COD_UNI"
 
     def self.buscar(codigo)
-      pagina = Nokogiri::HTML(open("#{WEBSRO}=#{codigo}"))
+      pagina = tracking_page(codigo)
 
       return false unless pagina.xpath("//tr").count > 0
 
@@ -60,6 +60,10 @@ class Correios
       end
 
       sro
+    end
+
+    def self.tracking_page(shipping_code)
+      Nokogiri::HTML(open("#{WEBSRO}=#{shipping_code}"))
     end
   end
 
